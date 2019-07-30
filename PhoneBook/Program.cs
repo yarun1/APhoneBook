9552d5efa1ce4db8884fea914236279b53b92004
Yarun <yarunkan@gmail.com>
2019-07-30 14:13:16 +0300

    Extend the functionality

--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.Serialization.Json;
 using System.Runtime.Serialization;
-
+using System.IO;
 
 namespace PhoneBook
 {
@@ -62,36 +62,100 @@ namespace PhoneBook
 
     class CreateContactHandler
     {
+        internal void CreateContact(Contact contacToCreate)
+        {
+            if (contacToCreate != null)
+            {
 
+            }
+            else
+            {
+                Console.WriteLine("'contacToCreate' references to null");
+            }
+        }
     }
 
     class EditContactHandler
     {
+        internal void EditContact(string contactData, Contact contactNewVersion)
+        {
+            if (!string.IsNullOrWhiteSpace(contactData) && contactNewVersion != null)
+            {
 
+            }
+            else
+            {
+                Console.WriteLine("No contact data or 'contactNewVersion' references to null");
+            }
+        }
     }
 
     class RemoveContactHandler
     {
+        internal void RemoveContact(string contactData, Contact contactNewVersion)
+        {
+            if (!string.IsNullOrWhiteSpace(contactData) && contactNewVersion != null)
+            {
 
+            }
+            else
+            {
+                Console.WriteLine("No contact data or 'contactNewVersion' references to null");
+            }
+        }
     }
 
     class ViewContactHandler
     {
+        internal void ViewContact()
+        {
 
+        }
     }
 
     class ContactSerialization
     {
+        string contactFileName = "contacts.json";
         DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Contact[]));
 
         internal void SerializeContact(Contact contacToSerialize)
         {
+            if (contacToSerialize != null)
+            {
+                using (FileStream fileStream = new FileStream("contacts.json", FileMode.OpenOrCreate))
+                {
+                    serializer.WriteObject(fileStream, contacToSerialize);
+                }
 
+                Console.WriteLine("A contact was successfully serialized");
+            }
+            else
+            {
+                Console.WriteLine("'contacToSerialize' references to null");
+            }
         }
 
         internal Contact[] DeserialzeContacts()
         {
+            if (new FileInfo(contactFileName).Length != 0)
+            {
+                Contact[] deserialzedContacts;
 
+                using (FileStream fileStream = new FileStream(contactFileName, FileMode.OpenOrCreate))
+                {
+                    deserialzedContacts = (Contact[])serializer.ReadObject(fileStream);
+                }
+
+                Console.WriteLine("A contact was successfully deserialized");
+
+                return deserialzedContacts;
+            }
+            else
+            {
+                Console.WriteLine("File can not be deserialized, because it is empty. Returning null...");
+
+                return null;
+            }
         }
     }
 
@@ -99,16 +163,15 @@ namespace PhoneBook
     class Contact
     {
         [DataMember]
-        public string ContactName { get; private set; }
+        internal string contactName;
 
         [DataMember]
-        public string ContactPhoneNumber { get; private set; }
-
+        internal string contactPhoneNumber;
 
         internal Contact(string contactName, string contactPhoneNumber)
         {
-            ContactName = contactName;
-            ContactPhoneNumber = contactPhoneNumber;
+            this.contactName = contactName;
+            this.contactPhoneNumber = contactPhoneNumber;
         }
     }
 }
